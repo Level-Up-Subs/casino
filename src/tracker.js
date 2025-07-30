2e71949a2d9ca3444255195f33c4a782a28df94d
Nicholas (Nico) Berriochoa <nberriochoa@gmail.com>
2025-07-29 21:22:06 -0600

    Update tracker.js

--- src/tracker.js
+++ src/tracker.js
@@ -1,1 +1,11 @@
-
+async function trackSubmission(subNumber)
+{
+  fetch("https://api.nberr.io/dealer", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ number: subNumber, key:<API_KEY_HERE> })
+})
+.then(res => res.json())
+.then(data => console.log("Backend response:", data))
+.catch(err => console.error("Error:", err));
+}
